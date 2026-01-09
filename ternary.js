468238bb53b85b6de9631d144f784b3e31e1b572
DG-Dibbo <dibboghosh2023@gmail.com>
2026-01-10 00:37:43 +0600

    add ternary operator

---
+++ ternary.js
@@ -0,0 +1,20 @@
+/*
+* TERNARY ---> Three parts
+* ______ ? ______: 
+*
+*Condition --> ? er por when condition is true do something --->: when condition is false do something ---
+*/
+
+const age = 20;
+
+// if( age > 18)
+// {
+//     console.log("Tumi vote dite parbe")
+// }
+// else
+// {
+//     console.log("Tumi vote dite parbe nh")
+// }
+// Use ternary operator
+// Simple ternary
+age > 18 ? console.log("Tumi vote dite parbe") : console.log("Tumi vote dite parbe nh")
