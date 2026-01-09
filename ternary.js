1571729253b70cf4d489e11bbb64e1ae640f0d76
DG-Dibbo <dibboghosh2023@gmail.com>
2026-01-10 00:44:33 +0600

    updated ternary

--- ternary.js
+++ ternary.js
@@ -5,8 +5,7 @@
 *Condition --> ? er por when condition is true do something --->: when condition is false do something ---
 */
 
-const age = 20;
-
+// const age = 20;
 // if( age > 18)
 // {
 //     console.log("Tumi vote dite parbe")
@@ -17,4 +16,18 @@ const age = 20;
 // }
 // Use ternary operator
 // Simple ternary
-age > 18 ? console.log("Tumi vote dite parbe") : console.log("Tumi vote dite parbe nh")
+// age > 18 ? console.log("Tumi vote dite parbe") : console.log("Tumi vote dite parbe nh")
+
+// let price = 500;
+// isleader = true;
+
+// if (isleader === true) {
+//     price = 0;
+// }
+// else {
+//     price = price + 100;
+// }
+// Use ternary
+// price = isleader === true ? 0 : price+100
+
+// console.log(price)
